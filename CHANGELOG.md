2f82355e213fb0293d7f1767e2e38e378941191b
Viktor Stanchev <me@viktorstanchev.com>
2013-02-13 22:18:00 -0500

    up the version to Alpha 0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -37,4 +37,7 @@
   * **Alpha 0.15 (2012-04-20):**
     * FIXED: 2 minor corrections to avoid notices in php log.
     * FIXED: Sources converted to UTF-8.
-
+  * **Alpha 0.2 (2012-04-20):**
+    * CHANGED: The entire backend was restrucured by Simon Rupf
+    * CHANGED: The entire frontend was converted to use Bootstrap by Cash Williams
+    * ADDED: File upload feature
--- lib/zerobin.php
+++ lib/zerobin.php
@@ -7,7 +7,7 @@
  * @link      http://sebsauvage.net/wiki/doku.php?id=php:zerobin
  * @copyright 2012 Sébastien SAUVAGE (sebsauvage.net)
  * @license   http://www.opensource.org/licenses/zlib-license.php The zlib/libpng License
- * @version   0.15
+ * @version   0.2
  */
 
 /**
@@ -20,7 +20,7 @@ class zerobin
     /*
      * @const string version
      */
-    const VERSION = 'Alpha 0.16';
+    const VERSION = 'Alpha 0.2';
 
     /**
      * @access private
